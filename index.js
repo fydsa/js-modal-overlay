195702feb898f7b0dfcc2dda630bf9173eca9de5
Saba <saba.alikhani@ymail.com>
2021-08-27 19:24:20 +0430

    Added header, body, and footer to the modal

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const button = document.querySelector("button");
 button.addEventListener("click", showModalHandler);
 
 let modal;
+let backdrop;
 
 function showModalHandler() {
   if (modal) {
@@ -12,18 +13,47 @@ function showModalHandler() {
   modal = document.createElement("div");
   modal.className = "modal";
 
+  const modalHeader = document.createElement("div");
+  modalHeader.className = "modal-header";
+
+  const modalBody = document.createElement("div");
+  modalBody.className = "modal-body";
+
+  const modalFooter = document.createElement("div");
+  modalFooter.className = "modal-footer";
+
+  const modalTitle = document.createElement("div");
+  modalTitle.textContent = "Modal Title";
+  modalTitle.className = "modal-title";
+
   const modalContent = document.createElement("p");
   modalContent.textContent = "Here is the simple modal overlay made with js!";
 
+  const modalActionYes = document.createElement("button");
+  const modalActionCancel = document.createElement("button");
   const modalClose = document.createElement("button");
 
+  modalActionYes.textContent = "Okay";
+  modalActionCancel.textContent = "Cancel";
   modalClose.textContent = "Close";
-  modalClose.className = "button button--alt";
 
+  modalActionYes.addEventListener("click", modalCloseHandler);
+  modalActionCancel.addEventListener("click", modalCloseHandler);
   modalClose.addEventListener("click", modalCloseHandler);
 
-  modal.append(modalContent);
-  modal.append(modalClose);
+  modalActionYes.className = "button button-okay";
+  modalActionCancel.className = "button button-cancel";
+  modalClose.className = "button-close";
+
+  modalHeader.append(modalTitle);
+  modalHeader.append(modalClose);
+  modalBody.append(modalContent);
+  modalFooter.append(modalActionCancel);
+  modalFooter.append(modalActionYes);
+
+  modal.append(modalHeader);
+  modal.append(modalBody);
+  modal.append(modalFooter);
 
   document.body.append(modal);
 
